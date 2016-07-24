3b98e87f0c8fa4ebf661945c9e8cfc46215a98c4
seelang2 <chris@sitebabble.com>
2016-07-23 20:02:26 -0500

    Added lodash to package.json

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# ses-imap
+# IMAP Server for Amazon Simple Email Service (SES)
+This module is an IMAP server to be used with Amazon SES.
+
+SES provides an SMTP server for outbound email; however, it doesn't include any service to process incoming email messages. Incoming email is stored in S3 buckets (which can be configured on a per-email-account basis).
+
+SES-IMAP uses [Imapper](https://www.npmjs.com/package/imapper) as the IMAP server, with [Imapper-Storage-S3SES](https://www.npmjs.com/package/imapper-storage-s3ses) and [Imapper-Auth-S3](https://www.npmjs.com/package/imapper-auth-s3) to access the message data and email account authentication credentials stored in S3.
+
+**NOTE** This module and the storage plugin is still under development and may not be suited to production use.
+
+## Usage
+
+imapper-auth-s3 uses the Amazon SDK. Make sure you have set up the SDK per the [Amazon JavaScript SDK Getting Started Guide](http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html).
+
+Download from the GitHub repository or use `npm install`:
+
+```sh
+npm install ses-imap
+```
+
+Start the server from the command line:
+
+```sh
+sudo node ses-imap --mbox-bucket maildata
+```
+
+### Configuration options
+The following parameters can be passed when starting the server:
+
+* `--mbox-bucket <bucketname>` : Required. The S3 bucket where the mailbox metadata is stored. No default value.
+* `--auth-suffix <suffix>` : Optional. The suffix added to the authentication file name. Default is '.auth.json'.
+* `--mbox-suffix <suffix>` : Optional. The account metadata file suffix. Default is '.mbox.json'.
+* `--mlist-suffix <suffix>` : Optional. The account message index file suffix. Default is  '.messagelist.json'.
+* `--mail-suffix <suffix>` : Optional. The suffix used for the account S3 bucket containing the messages. Default is '.ses.inbound'.
+* `--port <portnum>` : Optional. Port number the server listens on. Default is `143`.
+
+The following parameters are optional. Omitting them defaults the values to `false`, while using them sets them to `true`.
+
+* `--usetls` : Whether to support upgradable TLS connections. Relevant only if `--usessl` is not set.
+* `--usessl` : Default is `false`.
+* `--debug` : Verbose console output. Default is `false`.
+
+You can also specify the plugins Imapper should use. The STARTTLS and IDLE plugins are enabled by default. You can override what plugins by including the following parameters:
+
+`--plugin-id`, `--plugin-starttls`, `--plugin-sasl-ir`, `--plugin-auth-plain`, `--plugin-namespace`, `--plugin-idle`, `--plugin-enable`, `--plugin-condstore`, `--plugin-xtoybird`, `--plugin-literalplus`, `--plugin-unselect`, `--plugin-special-use`, `--plugin-create-special-use`
+
+For more information about the plugins or server features please check the [Imapper documentation](https://www.npmjs.com/package/imapper).
+
+
+## License 
+MIT
---
+++ ses-imap.js
@@ -0,0 +1,141 @@
+/**
+ * IMAP Server for use with Amazon Simple Email Service (SES)
+ *
+ * Notes:
+ * v0.1.0
+ * 		Added lodash to top level dependency in package.json as workaround for npm
+ *		not fully loading dependencies in sub branches
+ */
+
+var imapper = require("imapper");
+var userAuth = require("imapper-auth-s3");
+var storageModule = require("imapper-storage-s3ses");
+
+/* Parameterize the configuration vars */
+
+// extract command line argument list
+var params = process.argv;
+
+// if the first arg is node then remove the second arg which is the filename
+if (params.shift().toLowerCase() == 'node') params.shift();
+
+var arg, 
+		metadataBucket, 
+		authSuffix = '.auth.json',
+    mboxKeySuffix = '.mbox.json',
+    messageListKeySuffix = '.messagelist.json',
+    mboxBucketSuffix = '.ses.inbound',
+		port = 143, 
+		useTLS = false,
+		useSSL = false,
+		plugins = [],
+		debug = false;
+
+while (arg = params.shift()) {
+	switch(arg.toLowerCase()) {
+		case '--debug': 
+			debug = true;
+		break;
+		case '--mbox-bucket': 
+			metadataBucket = params.shift();
+		break;
+		case '--auth-suffix': 
+			authSuffix = params.shift();
+		break;
+		case '--mbox-suffix': 
+			mboxKeySuffix = params.shift();
+		break;
+		case '--mlist-suffix': 
+			messageListKeySuffix = params.shift();
+		break;
+		case '--mail-suffix': 
+			mboxBucketSuffix = params.shift();
+		break;
+		case '--port': 
+			port = params.shift();
+		break;
+		case '--usetls': 
+			useTLS = true;
+		break;
+		case '--usessl': 
+			useSSL = true;
+		break;
+		case '--plugin-id': 
+			plugins.push("ID");
+		break;
+		case '--plugin-starttls': 
+			plugins.push("STARTTLS");
+		break;
+		case '--plugin-sasl-ir': 
+			plugins.push("SASL-IR");
+		break;
+		case '--plugin-auth-plain': 
+			plugins.push("AUTH-PLAIN");
+		break;
+		case '--plugin-namespace': 
+			plugins.push("NAMESPACE");
+		break;
+		case '--plugin-idle': 
+			plugins.push("IDLE");
+		break;
+		case '--plugin-enable': 
+			plugins.push("ENABLE");
+		break;
+		case '--plugin-condstore': 
+			plugins.push("CONDSTORE");
+		break;
+		case '--plugin-xtoybird': 
+			plugins.push("XTOYBIRD");
+		break;
+		case '--plugin-literalplus': 
+			plugins.push("LITERALPLUS");
+		break;
+		case '--plugin-unselect': 
+			plugins.push("UNSELECT");
+		break;
+		case '--plugin-special-use': 
+			plugins.push("SPECIAL-USE");
+		break;
+		case '--plugin-create-special-use': 
+			plugins.push("CREATE-SPECIAL-USE");
+		break;
+	} // switch
+} // while arg
+
+// sanity checks 
+var errorMessage;
+if (typeof metadataBucket == 'undefined') errorMessage = 'No S3 metadata bucket defined.';
+if (typeof authSuffix == 'undefined') errorMessage = 'No authentication file suffix defined.';
+if (typeof mboxKeySuffix == 'undefined') errorMessage = 'No mailbox metadata file suffix defined.';
+if (typeof messageListKeySuffix == 'undefined') errorMessage = 'No message list file suffic';
+if (typeof mboxBucketSuffix == 'undefined') errorMessage = 'No mailbox bucket suffix defined';
+if (typeof port != 'number' || isNaN(port)) errorMessage = 'Port must be a valid number';
+
+if (typeof errorMessage != 'undefined') {
+	console.log('IMAP server initialization error:',errorMessage);
+	process.exit(); // terminate
+}
+
+// configure plugins
+userAuth.setS3Options('S3BucketName', metadataBucket);
+userAuth.setS3Options('S3KeySuffix', authSuffix);
+storageModule.setS3Options('S3BucketName', metadataBucket);
+storageModule.setS3Options('S3MboxKeySuffix', mboxKeySuffix);
+storageModule.setS3Options('S3MessageListKeySuffix', messageListKeySuffix);
+storageModule.setS3Options('S3MboxBucketSuffix', mboxBucketSuffix);
+storageModule.setS3Options('setConsoleMessages', debug);
+
+var options = {
+	tls: useTLS,
+	ssl: useSSL,
+	users: userAuth,
+	storage: storageModule,
+	plugins: plugins.length > 0 ? plugins: ["STARTTLS", "IDLE"],
+	debug: debug
+};
+
+console.log('Starting IMAP server with the following options:',options);
+
+var server = imapper(options);
+server.listen(port);
+console.log('IMAP server listening on port',port);
